7b7f5fd4f97d13d8464499aaa233dc61530469fa
Vickey Sri <vickeysavd@gmail.com>
2021-07-16 17:52:13 +0530

    Change base url to use this banner

---
+++ app/src/main/java/com/uniqueandrocode/myslider/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.uniqueandrocode.myslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SecondActivity extends AppCompatActivity {
+ImageView fullimage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+          fullimage=findViewById(R.id.fullimage);
+        Intent intent=getIntent();
+        String imageurl=intent.getStringExtra("imageurl");
+
+        Glide.with(this).load(imageurl).into(fullimage);
+    }
+}
---
+++ app/src/main/java/com/uniqueandrocode/myslider/api/MySliderApi.java
@@ -0,0 +1,16 @@
+package com.uniqueandrocode.myslider.api;
+
+
+import com.uniqueandrocode.myslider.model.MySliderList;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MySliderApi {
+
+
+    @GET("onbording.php")
+    Call<List<MySliderList>> getonbordingdata();
+}
